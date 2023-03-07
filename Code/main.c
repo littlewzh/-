d429a4d2479fe302a4d87f1c9878b3d0042429cf
littlewzh <201240080@smail.nju.edu.cn>
2023-03-07 16:41:02 +0800

    Linux little 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:41:02 up  6:12,  1 user,  load average: 1.09, 0.29, 0.10

---
+++ Code/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+extern FILE* yyin;
+extern int yylex();
+int main(int argc, char **argv){
+    if(argc>1){
+        if(!(yyin = fopen(argv[1],"r"))){
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    yylex();
+    return 0;
+}
