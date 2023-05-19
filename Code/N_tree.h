c3dc929a73c1c3c08677726aaec2deb0aad420b7
201240028-Zhu Yingjie <1477502024@qq.com>
2023-05-19 15:07:16 +0800

    zhu-vm 2023-05-19 15:07:16

---
+++ Code/N_tree.h
@@ -0,0 +1,30 @@
+#ifndef NTREE_H
+#define NTREE_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdarg.h>
+typedef struct treenode Tnode;
+typedef struct FieldList_ FieldList;
+struct treenode{
+	char name[16];
+	int line;
+	int type;
+	struct treenode *firstchild;
+	//struct treenode *parent;
+	struct treenode *nextbro;
+	FieldList *Syn;
+	FieldList *Inh;
+	union{
+		int i_val;
+		float f_val;
+		char *s_val;
+	};
+};
+Tnode *create_node(char *name,int type,int line);
+void insert_node(Tnode *p,Tnode* newn);
+void Ninsert(Tnode* p,int n,...);
+void dfs(Tnode* s,int dep);
+Tnode *childth_node(Tnode *p,int n);
+#endif
